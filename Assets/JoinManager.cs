9efd4a3fb9568d89a5732eb75394811383d5ba5a
23nekit <23nekit@gmail.com>
2020-08-03 17:55:26 +0300

    Initial Commit Add Prop Multiplayer 2 scenes with voice chat

---
+++ Assets/JoinManager.cs
@@ -0,0 +1,46 @@
+﻿using Photon.Pun;
+using Photon.Pun.Demo.Cockpit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinManager : MonoBehaviourPunCallbacks
+{
+    public InputField CreateRoomField;
+    public InputField JoinRoomField;
+    public Text TextFieldForFails;
+
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player" + Random.Range(100, 999);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void CreateRoom() 
+    {
+        PhotonNetwork.CreateRoom("k",new Photon.Realtime.RoomOptions { MaxPlayers=2});
+    }
+
+    public void LoadRoom() 
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+	{
+        TextFieldForFails.text = message;
+	}
+
+	void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/PlayerMoveCamera.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using Photon.Pun.Demo.PunBasics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveCamera : MonoBehaviour
+{
+	public OVRCameraRig MainCamera;
+
+    private PhotonView MyPhotonView;
+
+
+	private void Start()
+	{
+		MyPhotonView = GetComponent<PhotonView>();
+	}
+	void Update()
+    {
+		if (MyPhotonView.IsMine) 
+		{
+			MainCamera.disableEyeAnchorCameras = false;
+			if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickRight)) 
+			{
+				MainCamera.transform.eulerAngles = MainCamera.transform.eulerAngles + new Vector3(0, 0.1f, 0);
+			}
+			if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickLeft))
+			{
+				MainCamera.transform.eulerAngles = MainCamera.transform.eulerAngles + new Vector3(0, -0.1f, 0);
+			}
+			
+		}
+    }
+}
